d6e892d648e455da756c73e37ea03195240fe914
Malek <johnmalek54@gmail.com>
2023-07-05 12:55:55 +0300

    Change the directory where the files are saved

--- src/main/java/com/thrivematch/ThriveMatch/service/FilePathService.java
+++ src/main/java/com/thrivematch/ThriveMatch/service/FilePathService.java
@@ -11,7 +11,7 @@ public class FilePathService {
 
     public String saveFile(MultipartFile file) throws IOException{
         String fileName = file.getOriginalFilename();
-        String uploadDirectory = "/home/johnmalek/Desktop/Malek/SpringBoot/ThriveMatch/src/main/resources/static/Files/";
+        String uploadDirectory = "/home/johnmalek/Desktop/Malek/SpringBoot/ThriveMatch/src/main/java/com/thrivematch/ThriveMatch/files/";
         String filePath = uploadDirectory+fileName;
 
         file.transferTo(new File(filePath));
